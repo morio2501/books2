577aaab7d593268739dbf8a70e9a6d7cda2f520c
vagrant <morio4899@gmail.com>
2020-07-30 02:56:39 +0000

    root_top

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
 end
